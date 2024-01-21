7c38183c744330a55656f79d4ee8e6dfb7d09929
Sombressoul <Sombressoul@gmail.com>
2024-01-21 03:11:06 +0300

    Add some dev tests w/ notes

---
+++ dev_test_capacity.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from self_projection import SelfProjection
+
+# seeding
+torch.manual_seed(1)
+
+# Current:
+# 128x96x16 -> 128x8x8: seed -> 42:     1xSelfProjection    -> loss 0.041601695120334625
+# 128x96x16 -> 128x8x8: seed -> 1337:   2xSelfProjection    -> loss 0.03616994619369507
+
+# 128x16x96 -> 128x8x8: seed -> 313:    1xSelfProjection    -> loss 0.0419158972799778
+# 128x16x96 -> 128x8x8: seed -> 479:    2xSelfProjection    -> loss 0.03455526381731033
+
+input_shape = [96, 16]
+matrices_count = 128
+matrices_input = torch.rand([matrices_count, *input_shape]).to(
+    dtype=torch.float32, device="cuda"
+)
+projection_size = 16
+matrices_target = torch.rand([matrices_count, *([projection_size] * 2)]).to(
+    dtype=torch.float32, device="cuda"
+)
+epochs = 1.0e4
+
+
+class Net(nn.Module):
+    def __init__(
+        self,
+        i_size: list[int],
+        p_size: int,
+    ):
+        super(Net, self).__init__()
+        self.self_projection_a = SelfProjection(
+            size_input=i_size,
+            size_projection=p_size,
+        )
+        self.self_projection_b = SelfProjection(
+            size_input=[p_size, p_size],
+            size_projection=p_size,
+        )
+        pass
+
+    def forward(
+        self,
+        x: torch.Tensor,
+    ):
+        x = (x - x.mean(dim=[-1, -2], keepdim=True)) / (x.std() + 1.0e-5)
+        x = self.self_projection_a(x)[0]
+        x = self.self_projection_b(x)[0]
+        return x
+
+
+def train(model, optimizer, mat_input, mat_target, epochs):
+    for epoch in range(int(epochs)):
+        optimizer.zero_grad()
+        matrices_output = model(mat_input)
+        loss = F.mse_loss(matrices_output, mat_target)
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 100 == 0:
+            print(f"epoch {epoch} loss {loss.item()}")
+
+
+# train
+model = Net(input_shape, projection_size).to("cuda")
+optimizer = optim.Adam(model.parameters(), lr=1.0e-3)
+
+train(
+    model,
+    optimizer,
+    matrices_input,
+    matrices_target,
+    epochs,
+)
+
+# test
+matrices_output = model(matrices_input)
+
+print(f"Target 1-st:")
+print(matrices_target[0])
+print(f"Output 1-st:")
+print(matrices_output[0])
---
+++ dev_test_compression.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from self_projection import SelfProjection
+
+# seeding
+torch.manual_seed(2)
+
+# Current:
+# 128x64-128x16-128x64,     seed = 1,   loss = 0.07065999507904053
+# 256x128-256x32-256x128,   seed = 1,   loss = 0.0739060491323471
+# 256x128-256x64-256x128,   seed = 2,   loss = 0.05586353689432144
+
+
+input_size = 128
+projection_size = 64
+matrices_count = 256
+epochs = 1.0e4
+
+
+class Net(nn.Module):
+    def __init__(
+        self,
+        i_size: int,
+        p_size: int,
+    ):
+        super(Net, self).__init__()
+        self.self_projection_encode = SelfProjection(
+            size_input=[i_size] * 2,
+            size_projection=p_size,
+        )
+        self.self_projection_decode = SelfProjection(
+            size_input=[p_size] * 2,
+            size_projection=i_size,
+        )
+        pass
+
+    def forward(
+        self,
+        x: torch.Tensor,
+    ):
+        x = (x - x.mean(dim=[-1, -2], keepdim=True)) / (x.std() + 1.0e-5)
+        x = self.self_projection_encode(x)[0]
+        x = self.self_projection_decode(x)[0]
+        return x
+
+
+def train(model, optimizer, mat_input, mat_target, epochs):
+    for epoch in range(int(epochs)):
+        optimizer.zero_grad()
+        matrices_output = model(mat_input)
+        loss = F.mse_loss(matrices_output, mat_target)
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 100 == 0:
+            print(f"epoch {epoch} loss {loss.item()}")
+
+
+# train
+model = Net(input_size, projection_size).to("cuda")
+optimizer = optim.Adam(model.parameters(), lr=1.0e-3)
+
+matrices_input = torch.rand([matrices_count, *([input_size] * 2)]).to(
+    dtype=torch.float32, device="cuda"
+)
+matrices_target = matrices_input.clone()
+
+train(
+    model,
+    optimizer,
+    matrices_input,
+    matrices_target,
+    epochs,
+)
+
+# test
+matrices_output = model(matrices_input)
+
+print(f"Target 1-st:")
+print(matrices_target[0])
+print(f"Output 1-st:")
+print(matrices_output[0])
