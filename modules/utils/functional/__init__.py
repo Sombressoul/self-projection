38f8791b47552ebcb447269513df89635d5faa99
Sombressoul <Sombressoul@gmail.com>
2024-01-27 11:00:50 +0300

    Implement ChunkSplitter2D

--- modules/utils/functional/__init__.py
+++ modules/utils/functional/__init__.py
@@ -1,3 +1,4 @@
+from .chunk_splitter_2d import ChunkSplitter2D, ChunkSplitter2DMode
 from .count_trainable_params import count_trainable_params
 from .partial_norm import partial_norm
 from .standardize import standardize
@@ -5,6 +6,8 @@ from .rescale import rescale
 from .plot_loss import plot_loss
 
 __all__ = [
+    "ChunkSplitter2D",
+    "ChunkSplitter2DMode",
     "count_trainable_params",
     "partial_norm",
     "plot_loss",
